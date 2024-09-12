213c5725eff2e386a5906c78dcc43e0763b39bfc
Edwin Barreto <ebarreto@telesign.com>
2024-09-12 16:56:26 -0500

    modified:   environments/dev/main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,21 +1,21 @@
 module "vpc" {
-  source  = "../modules/vpc"
+  source  = "../../modules/vpc"
   vpc_cidr = var.vpc_cidr
 }
 
 module "iam" {
-  source = "../modules/iam"
+  source = "../../modules/iam"
 }
 
 module "alb" {
-  source         = "../modules/alb"
+  source         = "../../modules/alb"
   vpc_id         = module.vpc.vpc_id
   subnets        = module.vpc.subnet_ids
   security_group = module.security_groups.alb_security_group_id
 }
 
 module "ecs" {
-  source             = "../modules/ecs"
+  source             = "../../modules/ecs"
   cluster_name       = "ecs-cluster"
   execution_role_arn = module.iam.ecs_execution_role_arn
   subnets            = module.vpc.subnet_ids
@@ -26,7 +26,7 @@ module "ecs" {
 }
 
 module "security_groups" {
-  source  = "../modules/security_groups"
+  source  = "../../modules/security_groups"
   vpc_id  = module.vpc.vpc_id
 }
 
