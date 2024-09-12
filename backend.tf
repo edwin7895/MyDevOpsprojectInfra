ad4f8d8cfd8d8710a6281e356f9bd0cf4e5ad19f
Edwin Barreto <ebarreto@telesign.com>
2024-09-11 22:34:10 -0500

    dffgdf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,6 @@
-locals {
-  workspace_key = "${terraform.workspace}"
-}
-
 terraform {
   backend "s3" {
     bucket         = "edwinlabterraformstatebucket"
-    key            = "ecs-fargate/${local.workspace_key}/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "edwinlabterraformdynamo"
