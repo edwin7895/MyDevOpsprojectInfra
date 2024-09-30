498111c99f78bc893dd5e02ec9b319d0d1129b4b
Edwin Barreto <ebarreto@telesign.com>
2024-09-30 16:53:32 -0500

    route to internet added for vpc

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,21 +2,38 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-
 resource "aws_vpc" "ecs_vpc" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "public_subnet" {
-  count = 2
-  vpc_id = aws_vpc.ecs_vpc.id
-  cidr_block = cidrsubnet(var.vpc_cidr, 4, count.index)
+  count                   = 2
+  vpc_id                  = aws_vpc.ecs_vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index)
   map_public_ip_on_launch = true
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.ecs_vpc.id
 }
+
+# Tabla de rutas para la VPC que apunta al Internet Gateway
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.ecs_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Asociar la tabla de rutas con las subnets públicas
+resource "aws_route_table_association" "public_route_table_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
