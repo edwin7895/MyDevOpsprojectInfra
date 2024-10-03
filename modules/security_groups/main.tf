99a7590671dad12386c504f427658b3e50065e70
Edwin Barreto <ebarreto@telesign.com>
2024-10-03 16:24:52 -0500

    changes for the backend service

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -52,6 +52,13 @@ resource "aws_security_group" "ecs_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }  
 
+    ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # backend service - aws security group itself?
+  }    
+
   egress {
     from_port   = 0
     to_port     = 0
