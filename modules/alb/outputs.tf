198530ff33362c00f922bee1d7d045a5b0470499
Edwin Barreto <ebarreto@telesign.com>
2024-10-11 15:53:39 -0500

    alb output added

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -1,3 +1,8 @@
 output "app_tg_arn" {
   value = aws_lb_target_group.app_tg.arn
 }
+
+output "lb_dns_name" {
+  description = "El registro DNS asociado al Load Balancer"
+  value       = format("http://%s", aws_lb.app_alb.dns_name)
+}
