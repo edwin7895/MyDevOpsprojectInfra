646a6851ae22b62ce8250a92fee88dbdd07a69eb
Edwin Barreto <ebarreto@telesign.com>
2024-11-01 14:46:40 -0500

    gfg

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -2,6 +2,6 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "lb_dns_name" {
-  value       = module.alb.lb_dns_name
-}
+# output "lb_dns_name" {
+#   value       = module.alb.lb_dns_name
+# }
