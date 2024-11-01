ae3d767ae4d7199df67ea2b7a960971015699503
Edwin Barreto <ebarreto@telesign.com>
2024-11-01 15:21:10 -0500

    policy updated

--- environments/dev/lambda.tf
+++ environments/dev/lambda.tf
@@ -89,8 +89,54 @@ resource "aws_scheduler_schedule" "oddo_lambda_scheduler" {
   flexible_time_window {
     mode = "OFF"
   }
+  
   target {
     arn      = aws_lambda_function.oddo_lambda.arn
-    role_arn = aws_iam_role.oddo_lambda_role.arn
+    role_arn = aws_iam_role.eventbridge_scheduler_role.arn
   }
+}
+
+# IAM Role for EventBridge Scheduler
+resource "aws_iam_role" "eventbridge_scheduler_role" {
+  name = "eventbridge_scheduler_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# IAM Policy for EventBridge Scheduler to invoke Lambda
+resource "aws_iam_policy" "eventbridge_scheduler_policy" {
+  name        = "eventbridge_scheduler_execution_policy"
+  description = "Policy for EventBridge Scheduler to invoke Lambda functions"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "lambda:InvokeFunction"
+        ],
+        Resource = [
+          aws_lambda_function.oddo_lambda.arn
+        ]
+      }
+    ]
+  })
+}
+
+# Attach policy to the role
+resource "aws_iam_role_policy_attachment" "eventbridge_scheduler_policy_attachment" {
+  role       = aws_iam_role.eventbridge_scheduler_role.name
+  policy_arn = aws_iam_policy.eventbridge_scheduler_policy.arn
 }
